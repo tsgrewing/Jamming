5784caafb48a8a43b2750e7a09cb38c7f4e1542a
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-05-17 11:02:04 -0500

    adding searchbar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+import Playlist from '../Playlist';
+import SearchBar from '../SearchBar';
+import SearchResults from '../SearchResults';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
           <!-- Add a SearchBar component -->
-          <div className="App-playlist">
+          <div class="App-playlist">
             <!-- Add a SearchResults component -->
             <!-- Add a Playlist component -->
           </div>
