d3b8f094bc3c2f9d6110db86666934fda6e4b49b
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-05-18 13:19:49 -0500

    updating track list props

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,7 +6,14 @@ export class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                <!-- You will add a map method that renders a set of Track components  -->
+                {this.props.tracks.map(track => {
+                    (
+                        <div key={track.id}>
+                            <h3>{track.title}</h3>
+                            <p>{track.artist} | {track.album}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
